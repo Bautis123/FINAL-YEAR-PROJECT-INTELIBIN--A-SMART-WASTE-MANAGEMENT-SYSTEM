837228ccc78d0d189b94317ec9d5dc094b4176fa
Bautis Chileshe <chileshebautis@gmail.com>
2026-09-21 22:27:39 +0200

    Use recorded distance in recent readings

--- components/dashboard/recent_readings.php
+++ components/dashboard/recent_readings.php
@@ -16,8 +16,8 @@ $ib_rows = $ib_live ? array_slice(array_reverse($vm['history']), 0, 8) : [];
 <?php if (!$ib_rows): ?>
         <tr><td colspan="4" class="ib-none">No readings yet. Check that the Arduino is powered and connected. New readings will list here.</td></tr>
 <?php endif; ?>
-<?php foreach ($ib_rows as $ib_r): $ib_st = ib_status($ib_r['p'], (int)$vm['bin']['alert_at']); ?>
-        <tr data-ib-s="<?= $ib_st['key'] ?>"><td><?= ib_time($ib_r['t'], true) ?></td><td><?= round(ib_dist($ib_r['p'], $vm['bin'])) ?> cm</td><td><span class="ib-bar"><span style="width:<?= number_format($ib_r['p'], 0) ?>%"></span></span><?= number_format($ib_r['p'], 0) ?>%</td><td><span class="ib-pill ib-sm"><i></i><?= $ib_st['label'] ?></span></td></tr>
+<?php foreach ($ib_rows as $ib_r): $ib_st = ib_status($ib_r['p'], (int)$vm['bin']['alert_at']); $ib_d = $ib_r['d'] ?? ib_dist($ib_r['p'], $vm['bin']); ?>
+        <tr data-ib-s="<?= $ib_st['key'] ?>"><td><?= ib_time($ib_r['t'], true) ?></td><td><?= number_format($ib_d, 1) ?> cm</td><td><span class="ib-bar"><span style="width:<?= number_format($ib_r['p'], 0) ?>%"></span></span><?= number_format($ib_r['p'], 0) ?>%</td><td><span class="ib-pill ib-sm"><i></i><?= $ib_st['label'] ?></span></td></tr>
 <?php endforeach; ?>
       </tbody>
     </table>
